df658563895ed6b0e5c1a4f85064810d5d29e054
helios138 <73124365+helios138@users.noreply.github.com>
2022-06-10 17:24:42 +0800

    Reorg EventList

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import { handlers } from './lib/handlers'
 
 // In order for the workers runtime to find the class that implements
 // our Durable Object namespace, we must export it from the root module.
-// export { VoidEventList as EventList} from './lib/implementations/VoidEventList'
-export { WebSocketEventList as EventList } from './lib/implementations/WebSocketEventList'
+export { VoidEventList as EventList } from './lib/implementations/VoidEventList'
 
 export default handlers({
   interval: 6,
   functions: {
-    GET: ['getEvents', 'processEvents', 'websocket'],
+    GET: ['getEvents', 'processEvents'],
     POST: ['setup'],
   },
 })
