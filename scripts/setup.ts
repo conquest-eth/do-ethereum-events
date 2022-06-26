4aea09c75e39dbbb8bf4b26e795172c7725612e8
wighawag <wighawag@gmail.com>
2022-06-26 20:23:08 +0100

    fix setup

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import 'isomorphic-fetch';
 
 const args = (globalThis as any).process.argv.slice(2);
@@ -7,17 +6,19 @@ const dataFilepath = args[1];
 const reset = args[2] === 'reset' || args[3] === 'reset';
 const start = args[2] === 'start' || args[3] === 'start';
 
-const dataFromFile = JSON.parse(fs.readFileSync(dataFilepath).toString());
-
-let list: any;
-let all: any;
-if (Array.isArray(dataFromFile)) {
-  list = dataFromFile;
-} else {
-  all = dataFromFile;
-}
-
 async function main() {
+  const fsName = 'fs';
+  const fs = await import(fsName);
+  const dataFromFile = JSON.parse(fs.readFileSync(dataFilepath).toString());
+
+  let list: any;
+  let all: any;
+  if (Array.isArray(dataFromFile)) {
+    list = dataFromFile;
+  } else {
+    all = dataFromFile;
+  }
+
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify({
